e3ebcd441266998b7ba2bd260b7c1c6bcdfe136c
Pedro <pedrinhomerino@gmail.com>
2023-08-30 20:30:20 -0300

    local_load_current_account_test.dart

---
+++ test/data/usecases/load_current_account/local_load_current_account_test.dart
@@ -0,0 +1,50 @@
+import 'package:faker/faker.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tattoo_manager/data/data.dart';
+import 'package:tattoo_manager/domain/domain.dart';
+import 'package:test/test.dart';
+
+import '../../mocks/mocks.dart';
+
+void main() {
+  late LocalLoadCurrentAccount sut;
+  late SecureCacheStorageSpy secureCacheStorage;
+  late String token;
+
+  setUp(() {
+    token = faker.guid.guid();
+    secureCacheStorage = SecureCacheStorageSpy();
+    secureCacheStorage.mockFetch(token);
+    sut = LocalLoadCurrentAccount(fetchSecureCacheStorage: secureCacheStorage);
+  });
+
+  test('Should call FetchSecureCacheStorage with correct value', () async {
+    await sut.load();
+
+    verify(() => secureCacheStorage.fetch('token'));
+  });
+
+  test('Should return an AccountEntity', () async {
+    final AccountEntity account = await sut.load();
+
+    expect(account, AccountEntity(token: token));
+  });
+
+  test('Should throw UnexpectedError if FetchSecureCacheStorage throws',
+      () async {
+    secureCacheStorage.mockFetchError();
+
+    final Future<AccountEntity> future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
+  test('Should throw UnexpectedError if FetchSecureCacheStorage returns null',
+      () async {
+    secureCacheStorage.mockFetch(null);
+
+    final Future<AccountEntity> future = sut.load();
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+}
