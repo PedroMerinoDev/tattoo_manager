dbc6bd739767225dd1357a729f05219b3f6f3515
Pedro <pedrinhomerino@gmail.com>
2023-08-30 22:48:38 -0300

    getx splash presenter test

--- lib/presentation/presenters/getx_splash_presenter.dart
+++ lib/presentation/presenters/getx_splash_presenter.dart
@@ -1,20 +1,22 @@
+import 'package:get/get.dart';
+
 import '../../domain/usecases/usecases.dart';
 import '../../ui/pages/pages.dart';
 import '../mixins/mixins.dart';
 
-import 'package:get/get.dart';
-
-class GetxSplashPresenter extends GetxController with NavigationManager implements SplashPresenter {
+class GetxSplashPresenter extends GetxController
+    with NavigationManager
+    implements SplashPresenter {
   final LoadCurrentAccount loadCurrentAccount;
 
-  GetxSplashPresenter({ required this.loadCurrentAccount });
+  GetxSplashPresenter({required this.loadCurrentAccount});
 
   Future<void> checkAccount({int durationInSeconds = 2}) async {
     await Future.delayed(Duration(seconds: durationInSeconds));
     try {
       await loadCurrentAccount.load();
       navigateTo = '/surveys';
-    } catch(error) {
+    } catch (error) {
       navigateTo = '/login';
     }
   }
