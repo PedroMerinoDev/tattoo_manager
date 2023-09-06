c0f039347cd77ac5f3f0e127e5cbf49b5db5dc2d
Pedro <pedrinhomerino@gmail.com>
2023-09-06 17:15:28 -0300

    Estudio recebe body

--- lib/domain/usecases/estudio/estudio_08.dart
+++ lib/domain/usecases/estudio/estudio_08.dart
@@ -1,24 +1,71 @@
-import '../../entities/entities.dart';
+import 'package:equatable/equatable.dart';
 
-abstract class Estudio08 {
-  Future<EstudioEntity08> save({
-    required int userId01,
-    required String nomeEstudio,
-    String tipoPessoa,
-    String urlLogo,
-    String razaoSocial,
-    String cnpj,
-    String cep,
-    String endereco,
-    String bairro,
-    String estado,
-    String cidade,
-    String telefone1,
-    String telefone2,
-    String socialMidia,
-    String webSite,
-    String email,
-    double nota,
-    DateTime dataCadastro,
+import '../../entities/estudio/estudio.dart';
+
+abstract class AddEstudio {
+  Future<EstudioEntity08> add(AddEstudioParams params);
+}
+
+class AddEstudioParams extends Equatable {
+  final int userId01;
+  final String tipoPessoa;
+  final String nomeEstudio;
+  final String urlLogo;
+  final String razaoSocial;
+  final String cnpj;
+  final String cep;
+  final String endereco;
+  final String bairro;
+  final String estado;
+  final String cidade;
+  final String telefone1;
+  final String telefone2;
+  final String socialMidia;
+  final String webSite;
+  final String email;
+  final double nota;
+  final DateTime dataCadastro;
+
+  @override
+  List get props => [
+        userId01,
+        tipoPessoa,
+        nomeEstudio,
+        urlLogo,
+        razaoSocial,
+        cnpj,
+        cep,
+        endereco,
+        bairro,
+        estado,
+        cidade,
+        telefone1,
+        telefone2,
+        socialMidia,
+        webSite,
+        email,
+        nota,
+        dataCadastro,
+      ];
+
+  const AddEstudioParams({
+    required this.userId01,
+    required this.tipoPessoa,
+    required this.nomeEstudio,
+    required this.urlLogo,
+    required this.razaoSocial,
+    required this.cnpj,
+    required this.cep,
+    required this.endereco,
+    required this.bairro,
+    required this.estado,
+    required this.cidade,
+    required this.telefone1,
+    required this.telefone2,
+    required this.socialMidia,
+    required this.webSite,
+    required this.email,
+    required this.nota,
+    required this.dataCadastro,
   });
 }
